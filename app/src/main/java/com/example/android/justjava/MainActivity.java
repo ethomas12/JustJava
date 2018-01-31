8ebbe8bdaffd6f0cf2be4a2340d7b26df7b6ea9a
Elaine Thomas <el41net@gmail.com>
2018-01-30 17:35:26 -0700

    added increment buttons and simple methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,5 @@
 /**
- * IMPORTANT: Make sure you are using the correct package name. 
+ * IMPORTANT: Make sure you are using the correct package name.
  * This example uses the package name:
  * package com.example.android.justjava
  * If you get an error when copying this code into Android studio, update it to match teh package name found
@@ -9,11 +9,11 @@
 package com.example.android.justjava;
 
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 
@@ -32,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
@@ -43,12 +44,28 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
     /**
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
 
+    /**
+     * This method increments the quantity by 1.
+     */
+    public void increment (View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method decrements the quantity by 1.
+     */
+    public void decrement (View view) {
+        int quantity = 1;
+        display(quantity);
     }
 }
