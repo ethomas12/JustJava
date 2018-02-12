c93f1252cf14583e33ff2864e2e7bc7dacab66ab
Elaine Thomas <el41net@gmail.com>
2018-02-11 21:58:45 -0700

    name field

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
     String priceMessage = "";
+    String orderName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +42,18 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chocolateCheckbox = findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckbox.isChecked();
 
+        CheckBox catShitCheckbox = findViewById(R.id.cat_shit_checkbox);
+        boolean hasCatShit = catShitCheckbox.isChecked();
+
+        CheckBox butmegCheckbox = findViewById(R.id.butmeg_checkbox);
+        boolean hasButmeg = butmegCheckbox.isChecked();
+
+        EditText nameInputView = findViewById(R.id.name_input);
+        String orderName = nameInputView.getText().toString();
+
+
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, hasCatShit, hasButmeg, orderName);
 
         displayMessage(priceMessage);
     }
@@ -58,14 +70,19 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method creates the Order Summary
      *
-     * @param price cost of order
+     * @param price           cost of order
      * @param addWhippedCream add whipped cream true or false
+     * @param addChocolate    add chocolate true or false
+     * @param addCatShit      add cat shit true or false
+     * @param addButmeg       add butmeg tru or false
      * @return text summary
      */
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
-        String priceMessage = "Name: Si'mon deLeon";
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, boolean addCatShit, boolean addButmeg, String name) {
+        String priceMessage = "Name: " + name;
         priceMessage += "\nAdd whipped cream?  " + addWhippedCream;
         priceMessage += "\nAdd chocolate?  " + addChocolate;
+        priceMessage += "\nAdd cat shit?  " + addCatShit;
+        priceMessage += "\nAdd butmeg?  " + addButmeg;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal = $" + price;
         priceMessage += "\nThank you!";
